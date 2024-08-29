b44c2fcd7a4ad7eb5561160caf1d462e0d15987b
abadot1 <medvedmladshy@yandex.by>
2024-08-29 18:23:44 +0300

    new file:   public/img/logo.jpg 	new file:   src/App.css 	modified:   src/App.js 	modified:   src/components/Header.jsx 	deleted:    src/components/Menu.jsx 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <section>
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import Menu from "./Menu";
-import { BrowserRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        <BrowserRouter>
-          <Menu />
-        </BrowserRouter>
+        <header className="header">
+          <div className="logo"></div>
+          <nav className="nav_menu">
+            <ul>
+              <li>
+                <NavLink to="/home" className={setActive}>
+                  Главная
+                </NavLink>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <NavLink to="/about" className={setActive}>
+                  О нас
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
       </>
     );
   }
 }
+
+const setActive = ({ isActive }) => (isActive ? "nav__but-active" : "nav__but");
 
 export default Header;
