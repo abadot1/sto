c39c07d16eb53d17c51bcd53128b8335293a002c
abadot1 <medvedmladshy@yandex.by>
2024-08-31 21:35:07 +0300

    modified:   public/index.html 	modified:   src/components/Header.jsx 	new file:   src/img/map-pin.svg 	new file:   src/img/phone.svg 	new file:   src/img/viber.svg 	new file:   src/img/whatsapp.svg 	modified:   src/index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../img/logo.webp";
+import { ReactComponent as Viber } from "../img/viber.svg";
+import { ReactComponent as WhatsApp } from "../img/whatsapp.svg";
+import { ReactComponent as Phone } from "../img/phone.svg";
+import { ReactComponent as MapPin } from "../img/map-pin.svg";
 
 class Header extends React.Component {
   render() {
@@ -41,9 +45,34 @@ class Header extends React.Component {
                 </nav>
               </div>
               <div className="contacts__wrap">
+                <div className="num__wrap">
+                  <p>Контакты:</p>
+                  <a href="tel:+375293584596">
+                    <p>
+                      <Phone className="phone" />
+                      +375293584596
+                    </p>
+                  </a>
+                </div>
                 <div className="social__wrap">
-                  <div className="social__viber"></div>
-                  <div className="social__whatsApp"></div>
+                  <div className="social__item">
+                    <a href="viber://chat?number=%2B375293584596">
+                      <Viber className="viber" />
+                    </a>
+                  </div>
+                  <div className="social__item">
+                    <a href="https://api.whatsapp.com/send?phone=375293584596">
+                      <WhatsApp className="whatsApp" />
+                    </a>
+                  </div>
+                </div>
+                <div className="adress__wrap">
+                  <a href="https://yandex.by/maps/-/CDwmf63U">
+                    <p>
+                      <MapPin className="mapPin" />
+                      Кличев, ул.Пролетармкая, 104
+                    </p>
+                  </a>
                 </div>
               </div>
             </div>
